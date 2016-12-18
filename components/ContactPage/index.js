8c9072264d20e3ddf55ffaa65baa1bb6fa2e2214
manpenaloza <manuel.penaloza@gmx.at>
2016-12-18 11:14:07 +0100

    finalize v0.1 of contact page

--- components/ContactPage/index.js
+++ components/ContactPage/index.js
@@ -5,19 +5,24 @@ import PageWrapper from '../PageWrapper';
 import FullWidthBgImage from '../FullWidthBgImage';
 import SectionWrapper from '../SectionWrapper';
 
+
 class ContactPage extends React.Component {
 
   render() {
+    console.log(styles);
     const {route} = this.props;
     const post = route.page.data;
     const bgImage = require('./images/contatme-bg.jpg')
-    console.log(bgImage)
     return (
       <FullWidthBgImage image={bgImage} padding={20}>
         <PageWrapper>
           <PageHeadline color="#fff">{post.headline}</PageHeadline>
-          <SectionWrapper>
-            
+          <SectionWrapper customStyle="text-center">
+            <p className={styles.contactInfo}>I am availble for freelance work</p>
+            <a className={styles.messageMe} href="mailto:manuel.penaloza@gmx.at">Message me</a>
+            <p className={styles.contactInfo}>Or find me here.</p>
+            <a href="#" className={styles.socialIcon}><span className="fa fa-twitter"></span></a>
+            <a href="#" className={styles.socialIcon}><span className="fa fa-linkedin"></span></a>
           </SectionWrapper>
         </PageWrapper>
       </FullWidthBgImage>
