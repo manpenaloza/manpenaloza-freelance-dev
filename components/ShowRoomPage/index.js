87d053ca76203ddab8de2233daa2de85a4393a3c
manpenaloza <manuel.penaloza@gmx.at>
2016-12-18 08:15:14 +0100

    add showcase data

--- components/ShowRoomPage/index.js
+++ components/ShowRoomPage/index.js
@@ -6,9 +6,11 @@ import DocumentTitle from 'react-document-title'
 import access from 'safe-access'
 import { config } from 'config'
 import PageHeadline from '../PageHeadline';
+import PageWrapper from '../PageWrapper';
+import SectionWrapper from '../SectionWrapper';
 import GlassesSvg from './images/jsxIfyedGlassesSVG';
 import MtSvgLines from 'react-mt-svg-lines';
-import './style.css';
+import styles from './style.module.css';
 
 // TODO: REFACTOR STYLING/CLASSNAMES TO USE CSS MODULES
 class ShowRoomPage extends React.Component {
@@ -20,6 +22,33 @@ class ShowRoomPage extends React.Component {
     }
   }
 
+  renderReferencesWithSectionWrapper() {
+    let refs = require('./references.json');
+    let refItems = refs.map((ref) => {
+
+      return(
+        <figure key={ref.name} className={styles.refItemContainer}>
+          <a>
+            <img src={require(`./images/${ref.image}`)}/>
+            <div className={styles.refItemOverlay}>
+              <img src={require('./images/refItemHoverImg.png')}/>
+            </div>
+          </a>
+          <figcaption>
+            <strong>{ref.name}</strong>
+            <span>{ref.shortDescription}</span>
+          </figcaption>
+        </figure>
+      )
+    });
+
+    return(
+      <SectionWrapper customStyle={styles.showRoomItemsWrapper}>
+        {refItems}
+      </SectionWrapper>
+    )
+  }
+
   componentDidMount() {
     //new Vivus('glasses-svg', {duration: 200, file: <GlassesSvg/>});
   }
@@ -28,14 +57,15 @@ class ShowRoomPage extends React.Component {
     const {route} = this.props
     const post = route.page.data
     return (
-      <div>
+      <PageWrapper>
         <PageHeadline color="#44535A">Showroom</PageHeadline>
-        <div className="showRoomPageSectionWrapper glassesWrapper">
+        <SectionWrapper customStyle={styles.glassesWrapper}>
           <MtSvgLines animate={1400} duration={3000} fade={true}>
             <GlassesSvg/>
           </MtSvgLines>
-        </div>
-      </div>
+        </SectionWrapper>
+        {this.renderReferencesWithSectionWrapper()}
+      </PageWrapper>
     );
   }
 }
